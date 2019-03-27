3b6e5fbbe6e8df2ebe2f94db2aebf42d70a44ba7
Joshua Whitley <jwhitley@autonomoustuff.com>
2019-03-27 12:36:57 -0500

    Modifications to xsdValid.py suggested by @dirk-thomas.

--- xsdValid.py
+++ xsdValid.py
@@ -3,9 +3,8 @@
 import xmlschema
 
 format1 = xmlschema.XMLSchema('xsd/package_format1.xsd')
+print("'xsd/package_format1.xsd' file is valid.")
 format2 = xmlschema.XMLSchema('xsd/package_format2.xsd')
+print("'xsd/package_format2.xsd' file is valid.")
 format3 = xmlschema.XMLSchema('xsd/package_format3.xsd')
-
-print("Format1: ", format1.validity)
-print("Format2: ", format2.validity)
-print("Format3: ", format3.validity)
+print("'xsd/package_format3.xsd' file is valid.")
